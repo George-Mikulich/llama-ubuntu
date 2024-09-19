a5e310bddeb4667d12fc17a54cc8cdce490cb11f
George-Mikulich <g.mikulich@softteco.com>
2024-09-19 15:01:02 +0300

    add https redirect

--- caddy.sh
+++ caddy.sh
@@ -1,13 +1,20 @@
 sudo apt-get update
-sudo apt-get install certbot
-sudo apt-get install caddy
-sudo certbot certonly --standalone -d <domain>
-sudo cat <<EOF > /etc/caddy/caddyfile
+sudo apt-get install certbot --assume-yes
+sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt-get update
+sudo apt-get install caddy --assume-yes
+sudo certbot certonly --standalone -d llama.st-demo.com
+sudo cat <<EOF > /etc/caddy/Caddyfile
+:80 {
+        redir https://{host}{uri}
+}
 :443 {
-       tls /etc/letsencrypt/live/<domain>/fullchain.pem /etc/letsencrypt/live/<domain>/privkey.pem
-       reverse_proxy localhost:8080
-       log {
-           output file /var/log/caddy/access.log
-       }
-   }
+        tls /etc/letsencrypt/live/llama.st-demo.com/fullchain.pem /etc/letsencrypt/live/llama.st-demo.com/privkey.pem
+        reverse_proxy localhost:8080
+        log {
+                output file /var/log/caddy/access.log
+        }
+}
 EOF
